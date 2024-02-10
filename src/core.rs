26f4c25a97bef52a2111fb46576e263be6b374e3
Quaternions <krakow20@gmail.com>
2024-02-09 22:25:32 -0800

    try_evaluate

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub trait Zero{
 	fn zero()->Self;
 }
@@ -5,6 +7,25 @@ pub trait Zero{
 pub trait Evaluate<T>{
 	fn evaluate(&self)->T;
 }
+#[derive(Debug)]
+pub enum TryEvaluateError{
+	MissingUnknown(UnknownId),
+}
+impl std::fmt::Display for TryEvaluateError{
+	fn fmt(&self,state:&mut std::fmt::Formatter<'_>)->std::fmt::Result{
+		write!(state,"{self:?}")
+	}
+}
+impl std::error::Error for TryEvaluateError{}
+pub trait TryEvaluate<T>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>;
+}
+//TODO: implement TryEvaluate implicitly if the type implements Evaluate
+// impl<T,Eval:Evaluate<T>> TryEvaluate<T> for Eval{
+// 	fn try_evaluate(&self,_values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+// 		Ok(self.evaluate())
+// 	}
+// }
 pub trait Derivative{
 	type Derivative;
 	fn derivative(&self)->Self::Derivative;
@@ -14,6 +35,21 @@ pub trait Derivative{
 impl Zero for f32{
 	fn zero()->f32{
 		0.0
+	}
+}
+//i32
+impl Zero for i32{
+	fn zero()->i32{
+		0
+	}
+}
+
+//represents an unknown value.
+#[derive(Clone,Copy,Debug,Hash,id::Id,Eq,PartialEq)]
+pub struct UnknownId(u32);
+impl<T:Copy> TryEvaluate<T> for UnknownId{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		values.get(self).copied().ok_or(TryEvaluateError::MissingUnknown(*self))
 	}
 }
 
@@ -26,6 +62,12 @@ impl<T> Scalar<T>{
 impl<T:Copy> Evaluate<T> for Scalar<T>{
 	fn evaluate(&self)->T{
 		self.0
+	}
+}
+//TODO: make this implicit
+impl<T:Copy> TryEvaluate<T> for Scalar<T>{
+	fn try_evaluate(&self,_values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.evaluate())
 	}
 }
 impl<A:Zero> Derivative for Scalar<A>{
@@ -44,6 +86,11 @@ impl<A,B> Add<A,B>{
 impl<T:std::ops::Add<Output=T>,A:Evaluate<T>,B:Evaluate<T>> Evaluate<T> for Add<A,B>{
 	fn evaluate(&self)->T{
 		self.0.evaluate()+self.1.evaluate()
+	}
+}
+impl<T:std::ops::Add<Output=T>,A:TryEvaluate<T>,B:TryEvaluate<T>> TryEvaluate<T> for Add<A,B>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.0.try_evaluate(values)?+self.1.try_evaluate(values)?)
 	}
 }
 impl<A:Derivative,B:Derivative> Derivative for Add<A,B>{
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod core;
 #[cfg(test)]
 mod tests{
+	use std::collections::HashMap;
 	use crate::core::*;
 
 	#[test]
@@ -8,5 +9,14 @@ mod tests{
 		let sum=Add::new(Scalar::new(1.0),Scalar::new(2.0));
 		assert_eq!(sum.evaluate(),3.0);
 		assert_eq!(sum.derivative().evaluate(),0.0);
+	}
+	#[test]
+	fn unknown(){
+		let x=UnknownId::new(0);
+		let sum=Add::new(Scalar::new(1),x);
+		let mut environment=HashMap::new();
+		environment.insert(x,1);
+		assert_eq!(sum.try_evaluate(&environment).unwrap(),2);
+		//assert_eq!(sum.derivative().evaluate(),0.0);
 	}
 }
