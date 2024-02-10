f538f952d74f0985f0816a12aa5ce568b4db12e8
Quaternions <krakow20@gmail.com>
2024-02-10 00:26:34 -0800

    Minus, Power, Divide, Exp, Log

--- src/core.rs
+++ src/core.rs
@@ -180,6 +180,42 @@ impl<A,B,C> std::ops::Mul<C> for Plus<A,B>{
 	}
 }
 
+pub struct Minus<A,B>(A,B);
+impl<A,B> Minus<A,B>{
+	pub const fn new(a:A,b:B)->Self{
+		Self(a,b)
+	}
+}
+impl<T:std::ops::Sub<Output=T>,A:Evaluate<T>,B:Evaluate<T>> Evaluate<T> for Minus<A,B>{
+	fn evaluate(&self)->T{
+		self.0.evaluate()-self.1.evaluate()
+	}
+}
+impl<T:std::ops::Sub<Output=T>,A:TryEvaluate<T>,B:TryEvaluate<T>> TryEvaluate<T> for Minus<A,B>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.0.try_evaluate(values)?-self.1.try_evaluate(values)?)
+	}
+}
+impl<A:Derivative,B:Derivative> Derivative for Minus<A,B>{
+	type Derivative=Minus<A::Derivative,B::Derivative>;
+	fn derivative(&self,unknown_id:UnknownId)->Self::Derivative{
+		Minus(self.0.derivative(unknown_id),self.1.derivative(unknown_id))
+	}
+}
+//arithmetic
+impl<A,B,C> std::ops::Add<C> for Minus<A,B>{
+	type Output=Plus<Self,C>;
+	fn add(self,c:C)->Self::Output{
+		Plus(self,c)
+	}
+}
+impl<A,B,C> std::ops::Mul<C> for Minus<A,B>{
+	type Output=Times<Self,C>;
+	fn mul(self,c:C)->Self::Output{
+		Times(self,c)
+	}
+}
+
 pub struct Times<A,B>(A,B);
 impl<A,B> Times<A,B>{
 	pub const fn new(a:A,b:B)->Self{
@@ -213,6 +249,190 @@ impl<A,B,C> std::ops::Add<C> for Times<A,B>{
 	}
 }
 impl<A,B,C> std::ops::Mul<C> for Times<A,B>{
+	type Output=Times<Self,C>;
+	fn mul(self,c:C)->Self::Output{
+		Times(self,c)
+	}
+}
+
+pub struct Divide<A,B>(A,B);
+impl<A,B> Divide<A,B>{
+	pub const fn new(a:A,b:B)->Self{
+		Self(a,b)
+	}
+}
+impl<T:std::ops::Div<Output=T>,A:Evaluate<T>,B:Evaluate<T>> Evaluate<T> for Divide<A,B>{
+	fn evaluate(&self)->T{
+		self.0.evaluate()/self.1.evaluate()
+	}
+}
+impl<T:std::ops::Div<Output=T>,A:TryEvaluate<T>,B:TryEvaluate<T>> TryEvaluate<T> for Divide<A,B>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.0.try_evaluate(values)?/self.1.try_evaluate(values)?)
+	}
+}
+impl<A:Derivative+Copy,B:Derivative+Copy> Derivative for Divide<A,B>{
+	type Derivative=Minus<Divide<A::Derivative,B>,Divide<Times<A,B::Derivative>,Times<B,B>>>;
+	fn derivative(&self,unknown_id:UnknownId)->Self::Derivative{
+		Minus(Divide(self.0.derivative(unknown_id),self.1),Divide(Times(self.0,self.1.derivative(unknown_id)),Times(self.1,self.1)))
+	}
+}
+//arithmetic
+impl<A,B,C> std::ops::Add<C> for Divide<A,B>{
+	type Output=Plus<Self,C>;
+	fn add(self,c:C)->Self::Output{
+		Plus(self,c)
+	}
+}
+impl<A,B,C> std::ops::Mul<C> for Divide<A,B>{
+	type Output=Times<Self,C>;
+	fn mul(self,c:C)->Self::Output{
+		Times(self,c)
+	}
+}
+
+pub trait Pow<Rhs=Self>{
+	type Output;
+	fn pow(self,rhs:Rhs)->Self::Output;
+}
+impl Pow for f32{
+	type Output=Self;
+	fn pow(self,rhs:Self)->Self::Output{
+		self.powf(rhs)
+	}
+}
+impl Pow for i32{
+	type Output=Self;
+	fn pow(self,rhs:Self)->Self::Output{
+		self.pow(rhs as u32)
+	}
+}
+
+#[derive(Clone,Copy)]
+pub struct Power<A:Copy,B:Copy>(A,B);
+impl<A:Copy,B:Copy> Power<A,B>{
+	pub const fn new(a:A,b:B)->Self{
+		Self(a,b)
+	}
+}
+impl<T:Pow<Output=T>,A:Evaluate<T>+Copy,B:Evaluate<T>+Copy> Evaluate<T> for Power<A,B>{
+	fn evaluate(&self)->T{
+		self.0.evaluate().pow(self.1.evaluate())
+	}
+}
+impl<T:Pow<Output=T>,A:TryEvaluate<T>+Copy,B:TryEvaluate<T>+Copy> TryEvaluate<T> for Power<A,B>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.0.try_evaluate(values)?.pow(self.1.try_evaluate(values)?))
+	}
+}
+impl<A:Derivative+Copy,B:Derivative+Copy> Derivative for Power<A,B>{
+	type Derivative=Times<Self,Plus<Divide<Times<B,A::Derivative>,A>,Times<Log<A>,B::Derivative>>>;
+	fn derivative(&self,unknown_id:UnknownId)->Self::Derivative{
+		Times(*self,Plus(Divide(Times(self.1,self.0.derivative(unknown_id)),self.0),Times(Log(self.0),self.1.derivative(unknown_id))))
+	}
+}
+//arithmetic
+impl<A:Copy,B:Copy,C:Copy> std::ops::Add<C> for Power<A,B>{
+	type Output=Plus<Self,C>;
+	fn add(self,c:C)->Self::Output{
+		Plus(self,c)
+	}
+}
+impl<A:Copy,B:Copy,C:Copy> std::ops::Mul<C> for Power<A,B>{
+	type Output=Times<Self,C>;
+	fn mul(self,c:C)->Self::Output{
+		Times(self,c)
+	}
+}
+
+pub trait Logarithm{
+	type Output;
+	fn log(self)->Self::Output;
+}
+impl Logarithm for f32{
+	type Output=Self;
+	fn log(self)->Self::Output{
+		self.ln()
+	}
+}
+
+pub struct Log<A>(A);
+impl<A> Log<A>{
+	pub const fn new(a:A)->Self{
+		Self(a)
+	}
+}
+impl<T:Logarithm<Output=T>,A:Evaluate<T>> Evaluate<T> for Log<A>{
+	fn evaluate(&self)->T{
+		self.0.evaluate().log()
+	}
+}
+impl<T:Logarithm<Output=T>,A:TryEvaluate<T>> TryEvaluate<T> for Log<A>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.0.try_evaluate(values)?.log())
+	}
+}
+impl<A:Derivative+Copy> Derivative for Log<A>{
+	type Derivative=Divide<A,A::Derivative>;
+	fn derivative(&self,unknown_id:UnknownId)->Self::Derivative{
+		Divide(self.0,self.0.derivative(unknown_id))
+	}
+}
+//arithmetic
+impl<A,C> std::ops::Add<C> for Log<A>{
+	type Output=Plus<Self,C>;
+	fn add(self,c:C)->Self::Output{
+		Plus(self,c)
+	}
+}
+impl<A,C> std::ops::Mul<C> for Log<A>{
+	type Output=Times<Self,C>;
+	fn mul(self,c:C)->Self::Output{
+		Times(self,c)
+	}
+}
+
+pub trait Expable{
+	type Output;
+	fn exp(self)->Self::Output;
+}
+impl Expable for f32{
+	type Output=Self;
+	fn exp(self)->Self::Output{
+		self.exp()
+	}
+}
+
+pub struct Exp<A>(A);
+impl<A> Exp<A>{
+	pub const fn new(a:A)->Self{
+		Self(a)
+	}
+}
+impl<T:Expable<Output=T>,A:Evaluate<T>> Evaluate<T> for Exp<A>{
+	fn evaluate(&self)->T{
+		self.0.evaluate().exp()
+	}
+}
+impl<T:Expable<Output=T>,A:TryEvaluate<T>> TryEvaluate<T> for Exp<A>{
+	fn try_evaluate(&self,values:&HashMap<UnknownId,T>)->Result<T,TryEvaluateError>{
+		Ok(self.0.try_evaluate(values)?.exp())
+	}
+}
+impl<A:Derivative+Copy> Derivative for Exp<A>{
+	type Derivative=Times<Self,A::Derivative>;
+	fn derivative(&self,unknown_id:UnknownId)->Self::Derivative{
+		Times(Self(self.0),self.0.derivative(unknown_id))
+	}
+}
+//arithmetic
+impl<A,C> std::ops::Add<C> for Exp<A>{
+	type Output=Plus<Self,C>;
+	fn add(self,c:C)->Self::Output{
+		Plus(self,c)
+	}
+}
+impl<A,C> std::ops::Mul<C> for Exp<A>{
 	type Output=Times<Self,C>;
 	fn mul(self,c:C)->Self::Output{
 		Times(self,c)
