357bb1e2977b7fa9f5735f8e3ce57a50a540cf8d
Quaternions <krakow20@gmail.com>
2024-02-12 04:38:37 -0800

    replace environment with env

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,9 @@ mod tests{
 	fn try_evaluate(){
 		let x=VariableId::new(0);
 		let sum=Constant::new(2)+x;
-		let mut environment=HashMap::new();
-		environment.insert(x,1);
-		assert_eq!(sum.try_replace(&environment).unwrap().evaluate(),3);
+		let mut env=HashMap::new();
+		env.insert(x,1);
+		assert_eq!(sum.try_replace(&env).unwrap().evaluate(),3);
 		assert_eq!(sum.derivative(x).evaluate(),1);
 	}
 	#[test]
@@ -28,12 +28,12 @@ mod tests{
 		let x=gen.var();//VariableId::new(0);
 		let y=gen.var();//VariableId::new(1);
 		let expr=x+y*x+pow(x,y);
-		let mut environment=HashMap::new();
-		environment.insert(x,3.0);//x=3.0
-		environment.insert(y,5.0);//y=5.0
-		assert_eq!(expr.try_replace(&environment).unwrap().evaluate(),261.0);
-		assert_eq!(expr.derivative(x).try_replace(&environment).unwrap().evaluate(),411.0);
-		assert_eq!(expr.derivative(y).derivative(x).try_replace(&environment).unwrap().evaluate(),1174.938);
-		assert_eq!(expr.derivative(x).derivative(x).derivative(x).try_replace(&environment).unwrap().evaluate(),540.0);
+		let mut env=HashMap::new();
+		env.insert(x,3.0);//x=3.0
+		env.insert(y,5.0);//y=5.0
+		assert_eq!(expr.try_replace(&env).unwrap().evaluate(),261.0);
+		assert_eq!(expr.derivative(x).try_replace(&env).unwrap().evaluate(),411.0);
+		assert_eq!(expr.derivative(y).derivative(x).try_replace(&env).unwrap().evaluate(),1174.938);
+		assert_eq!(expr.derivative(x).derivative(x).derivative(x).try_replace(&env).unwrap().evaluate(),540.0);
 	}
 }
